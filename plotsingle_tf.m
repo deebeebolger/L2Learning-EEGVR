43cfc44940a235f34206113d5d020fe0f4ad4892
Deirdre Bolger <deirdre.bolger76@gmail.com>
2021-09-28 19:24:32 +0200

    Add function to display single time-frequency maps

---
+++ plotsingle_tf.m
@@ -0,0 +1,30 @@
+function plotsingle_tf(hdl,~)
+
+    D=get(hdl,'UserDat');
+    time = D{1,1};
+    timeindx = D{1,2};
+    freq = D{1,3};
+    powdata = D{1,4};
+    powlims = D{1,5};
+    currchan = D{1,6};
+    blstring = D{1,7};
+    cone_oi = D{1,8};
+    
+    f1=figure; set(f1,'PaperUnits','normalized','PaperPosition',[0.0235308 0.0272775 0.894169 0.909249],'Color',[1 1 1]);
+    
+    T = time(timeindx);
+    
+    ax = axes('parent',f1);
+    imagesc('Parent',ax,'XData',T,'YData',freq,'CData',powdata,'CDataMapping','scaled')
+    ax.XLim = [T(1) T(end)];
+    ax.YLim = [4 40];
+    ax.CLim = powlims;
+    ax.Layer = 'top';
+    
+    hold on
+    plot(ax,time,cone_oi,'w--','linewidth',2)
+    title([currchan,' : ',blstring]);
+    colormap(ax,jet);
+    colorbar;
+
+end
